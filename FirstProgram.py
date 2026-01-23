71d9f00238f7446f07e5e333542cb07333f8ed8f
miggy-al <03766542@nebraska.edu>
2026-01-23 02:23:24 +0000

    Completed Lab 1 programs

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,22 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Miguel Alvarado
+#Date: 1/22/2026
+#Assignment: Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+print("hello")
   #Ask for the user's name
-
+name= input("what is your name?")
   #Use the user's name in the program.
-
+print("hello", name)
   #Ask the user for their age.
-
+age= int(input("how old are you?"))
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-
-
+birthyear=2026-age-1
+print("you were born in", birthyear,name)
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
--- MadLib.py
+++ MadLib.py
@@ -1,13 +1,23 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Miguel Alvarado
+#Date: 1/22/2026
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+noun1 = input("Enter a noun: ")
+adjective1 = input("Enter a adjective: ")
+place1 = input("Enter a place: ")
+adjective2 = input("Enter a adjective: ")
+adjective3 = input("Enter a adjective: ")
+noun2 = input("Enter a noun: ")
   #Print the story with the user supplied words.
+print("I love to make" , noun1 + "!" , "They are very" , adjective1 +"!")
+print("I usually eat them at my", place1 +".")
+print("I like when it is", adjective2 + ".")
+print("I don't like when it is", adjective3 +".")
+print("Anyways thanks for listening my", noun2 +".")
 
 
 
